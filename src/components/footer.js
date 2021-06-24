72c75be611f3da67865f7867533c64511591aebc
Bhagya Lakshmi K <77100278+bhagya0896@users.noreply.github.com>
2021-06-24 10:01:44 +0530

    Create footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,31 @@
+import React,{useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+//react-bootstrap
+import {Container,Row,Card,Col} from 'react-bootstrap';
+
+
+function Footer ()
+{
+    
+    return(
+<>
+<Container fluid>
+    <Row >
+        <Col xs={12} md={12} className="text-center p-3">
+       <hr ></hr>
+           <small className=" text-center  text" style={{ fontSize: "11px",color:"black"}} >Copyright 2020 <b className="text"style={{color:"rgb(207, 23, 23)"}}>EduCo</b>. All rights reserved.</b></small>
+ 
+        </Col>
+    </Row>
+</Container>
+</>
+        
+       
+          
+   
+    )
+}
+
+export default  Footer;
